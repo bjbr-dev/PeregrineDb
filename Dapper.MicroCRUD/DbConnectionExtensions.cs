b632c5adf92e754995482f16f7b9afd21386ad5c
James Ratcliffe <james.ratcliffe@publishinteractive.com>
2016-12-29 20:15:53 +0000

    Empty projects

---
+++ Dapper.MicroCRUD/DbConnectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="DbConnectionExtensions.cs" company="Berkeleybross">
+// Copyright (c) Berkeleybross. All rights reserved.
+// </copyright>
+namespace Dapper.MicroCRUD
+{
+    using System.Data;
+
+    /// <summary>
+    /// Provides extensions to <see cref="IDbConnection"/> objects to assist in simple CRUD operations.
+    /// </summary>
+    public static class DbConnectionExtensions
+    {
+    }
+}
