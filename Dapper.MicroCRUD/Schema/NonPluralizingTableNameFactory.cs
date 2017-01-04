04aa2ad0352b1b53b1768d84dbb37514a34828c0
James Ratcliffe <james.ratcliffe@publishinteractive.com>
2017-01-04 22:55:26 +0000

    Added default table name factory which doesnt pluralize

---
+++ Dapper.MicroCRUD/Schema/NonPluralizingTableNameFactory.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="NonPluralizingTableNameFactory.cs" company="Berkeleybross">
+// Copyright (c) Berkeleybross. All rights reserved.
+// </copyright>
+namespace Dapper.MicroCRUD.Schema
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Implements the <see cref="ITableNameFactory"/> by using the <see cref="TableAttribute"/> if present,
+    /// otherwise just takes the class name.
+    /// </summary>
+    public class NonPluralizingTableNameFactory
+        : DefaultTableNameFactory
+    {
+        /// <inheritdoc />
+        protected override string GetTableNameFromType(Type type)
+        {
+            return type.Name;
+        }
+    }
+}
