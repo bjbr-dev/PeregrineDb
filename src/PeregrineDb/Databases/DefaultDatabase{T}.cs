d1b402be65cbb7b80bfe0fd5d4b36bcbd60de62c
James Ratcliffe <james.ratcliffe@publishinteractive.com>
2018-03-22 01:48:32 +0000

    Added overloads to default database to create generic transaction

--- src/PeregrineDb/Databases/DefaultDatabase{T}.cs
+++ src/PeregrineDb/Databases/DefaultDatabase{T}.cs
@@ -13,5 +13,37 @@
         }
 
         public new TConnection DbConnection { get; }
+
+        public IDatabaseUnitOfWork<TConnection, TTransaction> StartUnitOfWork<TTransaction>(bool leaveOpen = true)
+            where TTransaction : class, IDbTransaction
+        {
+            TTransaction transaction = null;
+            try
+            {
+                transaction = (TTransaction)this.DbConnection.BeginTransaction();
+                return DefaultUnitOfWork.From(this.DbConnection, transaction, this.Config, leaveOpen);
+            }
+            catch
+            {
+                transaction?.Dispose();
+                throw;
+            }
+        }
+
+        public IDatabaseUnitOfWork<TConnection, TTransaction> StartUnitOfWork<TTransaction>(IsolationLevel isolationLevel, bool leaveOpen = true)
+            where TTransaction : class, IDbTransaction
+        {
+            TTransaction transaction = null;
+            try
+            {
+                transaction = (TTransaction)this.DbConnection.BeginTransaction(isolationLevel);
+                return DefaultUnitOfWork.From(this.DbConnection, transaction, this.Config, leaveOpen);
+            }
+            catch
+            {
+                transaction?.Dispose();
+                throw;
+            }
+        }
     }
 }
--- src/PeregrineDb/IDatabase{T}.cs
+++ src/PeregrineDb/IDatabase{T}.cs
@@ -6,5 +6,28 @@
         : IDatabase, IDatabaseConnection<TConnection>
         where TConnection : IDbConnection
     {
+        /// <summary>
+        /// Starts a new <see cref="IDatabaseUnitOfWork"/> with the default <see cref="IsolationLevel"/>.
+        /// </summary>
+        /// <param name="leaveOpen">
+        /// When true (default), the created <see cref="IDatabaseUnitOfWork"/> will leave the underlying connection open when it is disposed.
+        /// When false, the created <see cref="IDatabaseUnitOfWork"/> will take ownership of the connection and dispose it when it itself is disposed.
+        /// This allows you to let the the current <see cref="IDatabase"/> instance fall out of scope safely.
+        /// </param>
+        /// <returns></returns>
+        IDatabaseUnitOfWork<TConnection, TTransaction> StartUnitOfWork<TTransaction>(bool leaveOpen = true)
+            where TTransaction : class, IDbTransaction;
+
+        /// <summary>
+        /// Starts a new <see cref="IDatabaseUnitOfWork"/> with the specified <paramref name="isolationLevel"/>.
+        /// </summary>
+        /// <param name="leaveOpen">
+        /// When true (default), the created <see cref="IDatabaseUnitOfWork"/> will leave the underlying connection open when it is disposed.
+        /// When false, the created <see cref="IDatabaseUnitOfWork"/> will take ownership of the connection and dispose it when it itself is disposed.
+        /// This allows you to let the the current <see cref="IDatabase"/> instance fall out of scope safely.
+        /// </param>
+        /// <returns></returns>
+        IDatabaseUnitOfWork<TConnection, TTransaction> StartUnitOfWork<TTransaction>(IsolationLevel isolationLevel, bool leaveOpen = true)
+            where TTransaction : class, IDbTransaction;
     }
 }
